1aebdd54776fbabd903598168d08478e00c64bf5
John Chapman <JayC202@gmail.com>
2015-09-16 22:18:22 -0400

    Added starting Siren point and gulp dependency

---
+++ Siren.js
@@ -0,0 +1,13 @@
+let Immutable = require('immutable');
+
+class Siren extends Immutable.Record({
+	classes: Immutable.List()
+	properties: Immutable.Map(),
+	entities: Immutable.Map(),
+	actions: Immutable.Map(),
+	links: Immutable.Map()
+}) {
+
+}
+
+module.exports = Siren;
